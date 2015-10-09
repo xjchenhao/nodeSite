4ff8cba84f8052724bade9dff4b87a20a90be137
xjchenhao <wy.chenhao@qq.com>
2015-10-09 13:53:45 +0800

    格式化导航添加的js代码

--- public/scripts/page/admin/manageNavAdd.js
+++ public/scripts/page/admin/manageNavAdd.js
@@ -1,42 +1,42 @@
 define(function (require, exports, module) {
     require('jquery');
 
-     var Verifyfrom=require('module/verifyfrom');
+    var Verifyfrom = require('module/verifyfrom');
 
-    var verifyfrom=new Verifyfrom({
-        isBlurVerify:true,
-        succeed:function(elm){
+    var verifyfrom = new Verifyfrom({
+        isBlurVerify: true,
+        succeed: function (elm) {
             $(elm).parent().addClass('has-success').removeClass('has-error').find('.text-danger').remove();
         },
-        failure:function(elm,errorMsg){
+        failure: function (elm, errorMsg) {
             $(elm).parent().addClass('has-error').removeClass('has-success')
                 .find('.text-danger').remove().end()
-                .append('<p class="text-danger">'+errorMsg+'</p>');
+                .append('<p class="text-danger">' + errorMsg + '</p>');
         }
     });
 
-    verifyfrom.add($('#projectName')[0],[{
-        strategy:'isNoEmpty',
-        errorMsg:'请输入项目名'
+    verifyfrom.add($('#projectName')[0], [{
+        strategy: 'isNoEmpty',
+        errorMsg: '请输入项目名'
     }]);
 
-    verifyfrom.add($('#describeCn')[0],[{
-        strategy:'isNoEmpty',
-        errorMsg:'请输入中文描述'
+    verifyfrom.add($('#describeCn')[0], [{
+        strategy: 'isNoEmpty',
+        errorMsg: '请输入中文描述'
     }]);
 
-    verifyfrom.add($('#describeEn')[0],[{
-        strategy:'isNoEmpty',
-        errorMsg:'请输入英文描述'
+    verifyfrom.add($('#describeEn')[0], [{
+        strategy: 'isNoEmpty',
+        errorMsg: '请输入英文描述'
     }]);
 
-    verifyfrom.add($('#link')[0],[{
-        strategy:'isNoEmpty',
-        errorMsg:'请输入超链接'
+    verifyfrom.add($('#link')[0], [{
+        strategy: 'isNoEmpty',
+        errorMsg: '请输入超链接'
     }]);
 
     $('#submitBtn').on('click', function () {
-        if(!verifyfrom.start()){
+        if (!verifyfrom.start()) {
             return false;
         }
         $('#manageNavForm').submit();
