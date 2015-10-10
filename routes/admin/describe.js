e33403afa6fb041487e41395b8d94d96578e7832
xjchenhao <wy.chenhao@qq.com>
2015-10-10 18:35:45 +0800

    markdown编辑器读取`md`文件

--- routes/admin/describe.js
+++ routes/admin/describe.js
@@ -2,13 +2,16 @@ var errorCatch = require('../../models/errorCatch');
 
 var manageModule = require('../../models/module');
 
+var fs=require("fs");
+
 module.exports = function (app) {
     app.route('/admin/describe')
         .get(function (req, res) {
             res.render('admin/describe', {
                 title: '项目描述',
-                doc:'',
-                catalogue: manageModule.catalogue
+                doc:fs.readFileSync("README.md","utf-8"),
+                catalogue: manageModule.catalogue,
+                breadcrumb: manageModule.getBreadcrumb(req)
             });
         });
 };
