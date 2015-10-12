11271768435cf335211fb5d637fafcfb3fc4bf28
xjchenhao <wy.chenhao@qq.com>
2015-10-12 11:39:48 +0800

    添加`markdown`修改项目描述功能

--- routes/project/describe.js
+++ routes/project/describe.js
@@ -5,6 +5,7 @@ var manageModule = require('../../models/module');
 var fs=require("fs");
 
 module.exports = function (app) {
+    //--------------------------------------------------【后台部分】
     app.route('/admin/describe')
         .get(function (req, res) {
             res.render('admin/project/describe', {
@@ -13,7 +14,16 @@ module.exports = function (app) {
                 catalogue: manageModule.catalogue,
                 breadcrumb: manageModule.getBreadcrumb(req)
             });
+        })
+        .post(function(req,res){
+            var bodyObj = req.body;
+
+            fs.writeFile('README.md',bodyObj.editor,function(){
+                res.redirect('/admin/describe');
+            });
         });
+
+    //--------------------------------------------------【前台部分】
     app.route(['/projectHelp'])
         .get(function (req, res) {
             res.render('project/describe', {
