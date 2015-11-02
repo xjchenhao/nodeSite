835f0047d69284d42398d849188543a20e969b24
xjchenhao <wy.chenhao@qq.com>
2015-11-02 15:03:35 +0800

    markdown编辑器入口模块名修改

--- public/scripts/config/adminMain.js
+++ public/scripts/config/adminMain.js
@@ -11,7 +11,7 @@ requirejs.config({
     shim: {
         'libs/bootstrap/dist/js/bootstrap.min': ['jquery'],
         'libs/metisMenu/dist/metisMenu.min': ['jquery'],
-        'module/markdown-editor': ['marked', 'libs/editor/vendor/codemirror', 'libs/editor/src/intro'],
+        'module/editor-markdown': ['marked', 'libs/editor/vendor/codemirror', 'libs/editor/src/intro'],
         'simditor':['jquery','libs/simple-module/lib/module','libs/simple-hotkeys/lib/hotkeys','libs/simple-uploader/lib/uploader']
     },
     map: {
