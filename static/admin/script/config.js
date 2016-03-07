ed947efd87231b8dbb150c3415bf8a54ba957b32
xjchenhao <wy.chenhao@qq.com>
2016-03-07 11:59:59 +0800

    细节优化

--- static/admin/script/config.js
+++ static/admin/script/config.js
@@ -1,34 +1,30 @@
-requirejs.config({
+require.config({
     baseUrl: '/admin/script',
     paths: {
-        module: 'module',
-        page: 'page',
-        libs: '../../../libs',
-
-        jquery: '../../../libs/jquery/dist/jquery',
-        bootstrap: '../../../libs/bootstrap/dist/js/bootstrap.min',
-        bootstrapTheme:'../../../libs/AdminLTE/dist/js/app.min',
-        metisMenu: '../../../libs/metisMenu/dist/metisMenu.min',
-        pace: '../../../libs/PACE/pace.min',
-        marked: '../../../libs/marked/lib/marked',
-        simditor: '../../../libs/simditor/lib/simditor'
+        jquery: '../../libs/jquery/dist/jquery',
+        bootstrap: '../../libs/bootstrap/dist/js/bootstrap.min',
+        bootstrapTheme:'../../libs/AdminLTE/dist/js/app.min',
+        fastclick:'../../libs/AdminLTE/plugins/fastclick/fastclick.min',
+        pace: '../../libs/PACE/pace.min',
+        marked: '../../libs/marked/lib/marked',
+        simditor: '../../libs/simditor/lib/simditor'
     },
     map: {
         '*': {
-            css: 'libs/require-css/css'
+            css: '../../libs/require-css/css'
         }
     },
     shim: {
         'bootstrap': {
-            deps: ['jquery', 'css!libs/font-awesome/css/font-awesome.min.css']
+            deps: ['jquery', 'css!../../libs/font-awesome/css/font-awesome.min.css']
         },
         'bootstrapTheme':['jquery','bootstrap'],
         'metisMenu': {
             deps: ['jquery']
         },
-        'pace':['css!libs/PACE/themes/blue/pace-theme-loading-bar.css'],
-        'module/editor-markdown': ['marked', 'libs/editor/vendor/codemirror', 'libs/editor/src/intro'],
-        'simditor': ['jquery', 'libs/simple-module/lib/module', 'libs/simple-hotkeys/lib/hotkeys', 'libs/simple-uploader/lib/uploader','css!libs/simditor/styles/simditor']
+        'pace':['css!../../libs/PACE/themes/blue/pace-theme-center-simple.css'],
+        'module/editor-markdown': ['marked', '../../../libs/editor/vendor/codemirror', '../../../libs/editor/src/intro'],
+        'simditor': ['jquery', '../../libs/simple-module/lib/module', '../../libs/simple-hotkeys/lib/hotkeys', '../../libs/simple-uploader/lib/uploader','css!../../libs/simditor/styles/simditor']
     }
 });
 
--- static/admin/script/page/base.js
+++ static/admin/script/page/base.js
@@ -2,47 +2,30 @@ define(function (require, exports, module) {
     require('bootstrap');
     require('bootstrapTheme');
 
+    //--------------------------------------------------【全局bug修复】
+    var fastclick=require('fastclick');
+
+    // 解决在微信上a标签:active伪类失效的bug
+    window.addEventListener('touchstart', function () {
+    }, false);
+
+    // fastclick代替zepto的tap,防止点透bug
+    fastclick.attach(document.body);
+
     //--------------------------------------------------【页面加载状态】
     var pace = require('pace');
+
     pace.start({
         document: false
     });
 
-    ////--------------------------------------------------【侧栏导航管理】
-    //require('metisMenu');
-    //
-    ////侧栏收缩
-    //$('#side-menu').metisMenu();
-    //
-    ////--------------------------------------------------【title提示框美化】
-    //$('[data-toggle="tooltip"]').tooltip();
-    //
-    ////--------------------------------------------------【自适应布局】
-    //$(window).on('resize', function () {
-    //    var topOffset = 50,
-    //        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-    //    if (width < 768) {
-    //        $('div.navbar-collapse').addClass('collapse');
-    //        topOffset = 100;
-    //    } else {
-    //        $('div.navbar-collapse').removeClass('collapse');
-    //    }
-    //
-    //    var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-    //    height = height - topOffset;
-    //    if (height < 1) height = 1;
-    //    if (height > topOffset) {
-    //        $('#page-wrapper').css('min-height', (height) + 'px');
-    //    }
-    //}).trigger('resize');
-    //
-    ////--------------------------------------------------【根据url展开侧栏导航】
-    //
-    //var url = window.location;
-    //$('#side-menu a').filter(function () {
-    //    return this.href == url || url.href.indexOf(this.href) == 0;
-    //})
-    //    .last().addClass('active')
-    //    .parents('li').addClass('active');
+    //--------------------------------------------------【根据url展开侧栏导航】
+
+    var url = window.location;
+    $('#sidebar-menu a').filter(function () {
+        return this.href == url || url.href.indexOf(this.href) == 0;
+    })
+        .last().addClass('active')
+        .parents('li').addClass('active');
 
 });
